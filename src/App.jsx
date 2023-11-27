21fe825d4a82346494b9882d97c4764f4f7a431d
Valen <gg172138406@gmail.com>
2023-11-27 16:22:24 +0800

    fix later

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,33 @@
 import Test from "./Test";
 import "./app.scss";
 import Navbar from "./components/navbar/Navbar";
-import Hero from './components/hero/Hero'
+import Hero from "./components/hero/Hero";
+import Parallax from "./components/parallax/Parallax";
+import Services from "./components/services/Services";
+import Portfolio from "./components/portfolio/Portfolio";
+import Contact from "./components/contact/Contact";
+import Cursor from "./components/cursor/Cursor";
 const App = () => {
   return (
     <div>
+      <Cursor />
       <section id="Homepage">
         <Navbar />
-        <Hero/>
+        <Hero />
       </section>
-      <section id="Services">Parallax</section>
-      <section>Services</section>
-      <section id="Portfolio">Parallax</section>
-      <section>Portfolio1</section>
-      <section>Portfolio2</section>
-      <section>Portfolio3</section>
-      <section id="Contact">Contact</section>
+      <section id="Services">
+        <Parallax type="services" />
+      </section>
+      <section>
+        <Services />
+      </section>
+      <section id="Portfolio">
+        <Parallax type="portfolio" />
+      </section>
+      <Portfolio />
+      <section id="Contact">
+        <Contact />
+      </section>
       {/* <Test />
     <Test /> */}
     </div>
---
+++ src/components/cursor/Cursor.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useState} from "react";
+import "./cursor.scss";
+import { motion } from "framer-motion";
+const Cursor = () => {
+  const [position, setPosition] = useState({ x: 0, y: 0 });
+
+  useEffect(() => {
+    const mouseMove = (e) => {
+      setPosition({ x: e.clientX, y: e.clientY });
+    };
+    window.addEventListener("mousemove", mouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", mouseMove);
+    };
+  },[]);
+  return (
+    <div>
+      <motion.div className="cursor" animate={{ x: position.x+10, y: position.y+10}}></motion.div>
+    </div>
+  );
+};
+
+export default Cursor;
